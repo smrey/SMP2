2ab302284d11b0605a9b2ac3f9dff823a9a16e48
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2017-08-02 17:31:24 +0100

    Added which samples read out to download as well

--- CRUK.sh
+++ CRUK.sh
@@ -174,7 +174,7 @@ function launchApp {
 			# Wait for the app to complete and store the appsession ID	
 			appRes=$(bs -c "$CONFIG" wait "$appSessionId" --terse)
 
-			echo "Downloading files"		
+			echo "Downloading files for ""$pair"		
 
 			# Download required analysis results files
 			bs cp conf://"$CONFIG"/Projects/"$projectId"/appresults/"$appRes"/*.bam "$RESULTSFOLDER"
