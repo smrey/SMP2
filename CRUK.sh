40bbf145ca6b16f704130e0b38371d1e36643bb6
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2017-08-03 12:32:14 +0100

    Removed automated file download to reduce script run time

--- CRUK.sh
+++ CRUK.sh
@@ -176,15 +176,6 @@ function launchApp {
 			# Launch app and store the appsession ID	
 			appSessionId=$(bs -c "$CONFIG" launch app -i "$APPID" "$negId" "$norId" "$projectName" "$tumId" --terse)
 	
-			# Wait for the app to complete and store the appsession ID	
-			appRes=$(bs -c "$CONFIG" wait "$appSessionId" --terse)
-
-			echo "Downloading files for ""$pair"		
-
-			# Download required analysis results files
-			bs cp conf://"$CONFIG"/Projects/"$projectId"/appresults/"$appRes"/*.bam "$RESULTSFOLDER"
-			bs cp conf://"$CONFIG"/Projects/"$projectId"/appresults/"$appRes"/*.bai "$RESULTSFOLDER"
-			bs cp conf://"$CONFIG"/Projects/"$projectId"/appresults/"$appRes"/*.xls* "$RESULTSFOLDER"
 
 	done <"$SAMPLEPAIRS"
 
