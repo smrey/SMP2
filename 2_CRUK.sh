540e4b5e44354653072f61caf38498a923818b3a
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2017-08-25 17:08:32 +0100

    Added additional script intended for download

---
+++ 2_CRUK.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+
+#Description: CRUK Basespace app pipeline
+#Author: Sara Rey
+#Status: DEVELOPMENT/TESTING
+Version=0.1
+
+# Aliases for local python VE
+alias python='/home/transfer/basespace_vm/venv/bin/python'
+PATH="$PATH":/home/transfer/basespace_vm/venv/bin/
+
+# How to use
+# bash 2_CRUK.sh
+
+# Variables
+CONFIG=
+projectId=
+appResultsId=
+
+
