9308a345a2f69980bb9535b1a4821acd892f7ce1
Sara Rey <sara.rey@postgrad.manchester.ac.uk>
2017-09-22 14:31:23 +0100

    adapter trimming script added

--- 1_CRUK.sh
+++ 1_CRUK.sh
@@ -1,232 +1,90 @@
 #!/bin/bash
+#PBS -l walltime=20:00:00
+#PBS -l ncpus=12
 set -euo pipefail
+PBS_O_WORKDIR=(`echo $PBS_O_WORKDIR | sed "s/^\/state\/partition1//" `)
+cd $PBS_O_WORKDIR
 
-#Description: CRUK Basespace app pipeline
-#Author: Sara Rey
-#Status: DEVELOPMENT/TESTING
-Version=2.0
+#Description: CRUK Pipeline (Illumina paired-end). Not for use with other library preps/ experimental conditions.
+#Mode: BY_SAMPLE
+version="1.0.0"
 
-# Aliases for local python VE
-alias python='/home/transfer/basespace_vm/venv/bin/python'
-PATH="$PATH":/home/transfer/basespace_vm/venv/bin/
+# Directory structure required for pipeline
+#
+# /data
+# └── results
+#     └── seqId
+#         ├── panel1
+#         │   ├── sample1
+#         │   ├── sample2
+#         │   └── sample3
+#         └── panel2
+#             ├── sample1
+#             ├── sample2
+#             └── sample3
+#
+# Script 1 runs in sample folder, requires fastq files split by lane
 
-# How to use
-# bash 1_CRUK.sh <path_to_sample_sheet> <name_of_negative_control_sample> <sample_pairs_text_file (optional)>
-
-# Variables
-CONFIG="pmg-euc1"
-APPID="91091"
-NOTBASESPACE="not_bs.txt"
-
-
-# Usage checking
-if [ "$#" -lt 2 ]
-	then
-		echo "Commandline args incorrect. Usage: $0 <path_to_sample_sheet> <name_of_negative_control_sample> <sample_pairs_text_file (optional)>." 
-		exit -1
-fi
-
-# Variables dependent on command line arguments
-INPUTFOLDER="$1"
-NEGATIVE="$2"
-FASTQFOLDER="$INPUTFOLDER""/Data/*/"
-
-# Check if file containing sample pairs has been supplied or if one should be automatically generated
-if [ "$#" -lt 3 ]
-	then
-		SAMPLEPAIRS="$INPUTFOLDER""SamplePairs.txt"
-		makePairs=1
-	else
-		SAMPLEPAIRS="$3"
-		# Skip generation of a SamplePairs.txt file
-		makePairs=-1
-fi
-
-
-# Check for the presence of the file with samples not to upload to BaseSpace in the same directory as the script
-if [[ -e $NOTBASESPACE ]]
-	then
-		samples_to_skip=1
-		# Check that the provided file is not empty
-		if ! [[ -s $NOTBASESPACE ]]
-			then
-				echo "The file "$NOTBASESPACE" is empty. When this file exists, it must contain the names of samples that are in the SampleSheet.csv, but should not be uploaded to BaseSpace."
-				exit -1
-		fi
-	else
-		samples_to_skip=-1
-		# Notify the user that all samples in the sample sheet will be uploaded
-		echo "No "$NOTBASESPACE" file found in the same directory as the script. All samples on the SampleSheet.csv will be uploaded to BaseSpace."
-fi
-
-
-# Declare an array to store the sample ids in order
-declare -a samplesArr
-# Initial entry created to avoid downstream error when appending to array
-samplesArr+=1 
-
-
-# Parse SampleSheet
-function parseSampleSheet {
-
-	echo "Parsing sample sheet"
-	
-	# Obtain project name from sample sheet
-	projectName=$(grep "Experiment Name" "$INPUTFOLDER""SampleSheet.csv" | cut -d, -f2 | tr -d " ")
-
-	# Obtain list of samples from sample sheet
-	for line in $(sed "1,/Sample_ID/d" "$INPUTFOLDER""SampleSheet.csv" | tr -d " ")
-		do 
-			# Obtain sample name and patient name		
-			samplename=$(printf "$line" | cut -d, -f1 | sed 's/[^a-zA-Z0-9]+/-/g')
-
-	 		# Skip any empty sample ids- both empty and whitespace characters (but not tabs at present)
-	 		if [[ "${#samplename}" = 0 ]] || [[ "$samplename" =~ [" "] ]]
-				then
-					continue
-	 		fi
-
-			# Append information to list array- to retain order for sample pairing
-			samplesArr=("${samplesArr[@]}" "$samplename")
-	done
+countQCFlagFails() {
+    #count how many core FASTQC tests failed
+    grep -E "Basic Statistics|Per base sequence quality|Per tile sequence quality|Per sequence quality scores|Per base N content" "$1" | \
+    grep -v ^PASS | \
+    grep -v ^WARN | \
+    wc -l | \
+    sed 's/^[[:space:]]*//g'
 }
 
+#load sample & pipeline variables
+. *.variables
+. /data/diagnostics/pipelines/CRUK/CRUK-"$version"/"$panel"/"$panel".variables
 
-function pairSamples {
+### Preprocessing ###
 
-	echo "Pairing samples"
+#record FASTQC pass/fail
+rawSequenceQuality=PASS
 
-	# Create/clear file which holds the sample name and the patient identifiers
-	> "$SAMPLEPAIRS"
-	
-	# Iterate through the samples and exclude any samples that are not for basespace
-	# Pair the samples assuming the order tumour then normal and create a file of these pairs
-	# Create array containing the samples that are not tumour-normal pairs
-	# Check if there are any samples on the run that are not for BaseSpace and so should not be paired
-	if [[ -e $NOTBASESPACE ]]
-		then
-			mapfile -t notPairs < $NOTBASESPACE
-			notPairs=("${notPairs[@]}" "$NEGATIVE") 
-		else
-			notPairs+=("$NEGATIVE")
-	fi	
-	
-	# Exclude non tumour-normal pairs from pair file creation		
-	grep -f <(printf -- '%s\n' "${notPairs[@]}") -v <(printf '%s\n' "${samplesArr[@]:1}") | awk -F '\t' 'NR % 2 {printf "%s\t", $1;} !(NR % 2) {printf "%s\n", $1;}' >"$SAMPLEPAIRS"
+#convert FASTQ to uBAM & add RGIDs
+for fastqPair in $(ls "$sampleId"_S*.fastq.gz | cut -d_ -f1-3 | sort | uniq); do
 
-}
+    #parse fastq filenames
+    laneId=$(echo "$fastqPair" | cut -d_ -f3)
+    read1Fastq=$(ls "$fastqPair"_R1_*fastq.gz)
+    read2Fastq=$(ls "$fastqPair"_R2_*fastq.gz)
 
+    #trim adapters
+    /share/apps/cutadapt-distros/cutadapt-1.9.1/bin/cutadapt \
+    -a "$read1Adapter" \
+    -A "$read2Adapter" \
+    -m 35 \
+    -o "$seqId"_"$sampleId"_"$laneId"_R1.fastq.gz \
+    -p "$seqId"_"$sampleId"_"$laneId"_R2.fastq.gz \
+    "$read1Fastq" \
+    "$read2Fastq"
 
-function locateFastqs {
+    #fastqc
+    /share/apps/fastqc-distros/fastqc_v0.11.5/fastqc -d /state/partition1/tmpdir --threads 12 --extract "$seqId"_"$sampleId"_"$laneId"_R1.fastq.gz
+    /share/apps/fastqc-distros/fastqc_v0.11.5/fastqc -d /state/partition1/tmpdir --threads 12 --extract "$seqId"_"$sampleId"_"$laneId"_R2.fastq.gz
 
-	echo "Uploading fastqs"
+    #check FASTQC output
+    if [ $(countQCFlagFails "$seqId"_"$sampleId"_"$laneId"_R1_fastqc/summary.txt) -gt 0 ] || [ $(countQCFlagFails "$seqId"_"$sampleId"_"$laneId"_R2_fastqc/summary.txt) -gt 0 ]; then
+        rawSequenceQuality=FAIL
+    fi
 
-	if [[ "$samples_to_skip" == 1 ]]
-		then
-			fastqlist=$( printf -- '%s\n' "${samplesArr[@]:1}" | grep -f "$NOTBASESPACE" -v )
-		else
-			fastqlist=$(printf -- '%s\n' "${samplesArr[@]:1}")
-	fi
-	
-	for fastq in $(printf -- '%s\n' "$fastqlist")
-		do
-			f1=$FASTQFOLDER${fastq}*_R1_*.fastq.gz
-			f2=$FASTQFOLDER${fastq}*_R2_*.fastq.gz
-		
-			# Obtain basespace identifier for each sample
-			baseSpaceId=$(bs -c "$CONFIG" upload sample -p $projectName -i "$fastq" $f1 $f2 --terse)
-	done
+    #print fastq paths <path2r1> <path2r2>
+    echo -e "$(find $PWD -name $seqId_$sampleId_$laneId_R1.fastq.gz)\t$(find $PWD -name $seqId_$sampleId_$laneId_R2.fastq.gz)" >> ../FASTQs.list
 
-}
+done
 
+#Print QC metrics
+echo -e "RawSequenceQuality" > "$seqId"_"$sampleId"_QC.txt
+echo -e "$rawSequenceQuality" >> "$seqId"_"$sampleId"_QC.txt
 
-function launchApp {
-
-	# Launch app for each pair of samples in turn as tumour normal pairs then download analysis files
-	
-	# Obtain basespace ID of negative control- this is not an optional input through the commandline app launch
-	negId=$(bs -c "$CONFIG" list samples --project "$projectName" --sample "$NEGATIVE" --terse)
-
-	# Obtain the project identifier
-	projectId=$(bs -c "$CONFIG" list projects --project-name "$projectName" --terse)
-
-	while read pair
-		do
-			# Stop iteration on first empty line of SamplePairs.txt file in case EOF marker is absent for any reason
-			if [[ -z $pair ]]
-				then
-					exit
-			fi
-			echo "Launching app for ""$pair"
-			
-			tum=$(printf "$pair" | cut -d$'\t' -f1)
-			nor=$(printf "$pair" | cut -d$'\t' -f2)
-
-			# Obtain sample ids from basespace
-			tumId=$(bs -c "$CONFIG" list samples --project "$projectName" --sample "$tum" --terse)
-			norId=$(bs -c "$CONFIG" list samples --project "$projectName" --sample "$nor" --terse)
-
-			# Launch app and store the appsession ID	
-			appSessionId=$(bs -c "$CONFIG" launch app -i "$APPID" "$negId" "$norId" "$projectName" "$tumId" --terse)
-	done <"$SAMPLEPAIRS"
-
-}
-
-
-
-# Call the functions
-
-#Check sample sheet exists at location provided
-if ! [[ -e "$INPUTFOLDER""SampleSheet.csv" ]]
-	then
-		echo "Sample Sheet not found at input folder location"
-		exit -1
+#check if all samples are written
+if [ $(find .. -maxdepth 1 -mindepth 1 -type d | wc -l | sed 's/^[[:space:]]*//g') -eq $(sort ../FASTQs.list | uniq | wc -l | sed 's/^[[:space:]]*//g') ]; then
+    echo -e "seqId=$seqId\npanel=$panel" > ../variables
+    
+    #launch BS script
+    ln -s /data/archive/fastq/"$seqId"/SampleSheet.csv
+    ###TODO###
+    cp launch_script.sh .. && qsub launch_script.sh
 fi
-
-
-# Parse sample sheet to obtain required information
-parseSampleSheet
-
-
-# Pair samples according to order in sample sheet if manually created pairs file has not been supplied
-if [[ "$makePairs" == 1 ]]
-	then
-		pairSamples
-fi
-
-# Count number of paired samples
-numPairs=$(cat "$SAMPLEPAIRS" | cut -f2 | wc -l)
-
-# Read out the sample pairs in the order tumour blood with each pair on a new line 
-echo "Displaying sample pairs:" 
-cat "$SAMPLEPAIRS"
-printf $'\n'
-echo "Abort the script if the samples are paired incorrectly and create a file of the pairs (see README.MD for details about this file)." 
-printf $'\n'
-
-
-# Create project in basespace
-echo "Creating project"
-bs -c "$CONFIG" create project "$projectName"
-
-
-# Get fastqs and upload to basespace
-locateFastqs
-
-
-# Kick off the app for each pair in turn and download files
-launchApp
-
-# Write config file for JavaScript script
-printf '%s\n' "{" "\"projectID\": ""\"$projectId\"""," "\"numPairs\": ""\"$numPairs\"""," "\"negativeControl\": ""\"$NEGATIVE\""  "}" > runConfig.json
-
-
-# Delete the file that contained the samples that were not for upload and analysis in BaseSpace
-if [[ -e $NOTBASESPACE ]]
-	then
-		rm "$NOTBASESPACE"
-fi
-
-
-# Queue next script in the pipeline for half an hours time
-at now +30 minutes -f ./2_CRUK.sh
